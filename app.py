7dbf51c5c0906fcf6538a59cfd1481f2afca701a
ITexperts-sanju <sanjay.saregama@gmail.com>
2025-09-12 15:53:57 +0530

    next step

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Kubernetes! 🎉"
+    return "Hello this is out first devops pipeline! 🎉"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
